97dd22bbe67bffc1612935aec354d90dffc455d0
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2023-05-25 10:13:15 +0300

    Done

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,13 +1,16 @@
 describe User do 
   let!(:user) { User.new }
-
+  describe '#first_name' do
   it "has a first name" do 
     user.first_name = "Tracy"
     expect(user.first_name).to eq("Tracy")
   end
+end
 
-  it "has a last name" do 
+  describe '#last_name' do
+    it "has a last name" do 
     user.last_name = "Morgan"
     expect(user.last_name).to eq("Morgan")
+    end
   end
 end
